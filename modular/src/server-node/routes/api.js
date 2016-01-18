ecb375c9084d9b0fd68d7c1bbfe3a521d0eb0288
Russ Watson <roustalski@hotmail.com>
2016-01-18 15:13:30 -0600

    Copied over data and service API from the angular demo

--- modular/src/server-node/routes/api.js
+++ modular/src/server-node/routes/api.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var jsonfileservice = require('./utils/jsonfileservice')();
 
-router.get('/', function(req, res, next) {
-    res.json({sample: "json"});
+router.get('/maa', function (req, res, next) {
+    var json = jsonfileservice.getJsonFromFile('/../../data/maa.json');
+    json[0].data.results.forEach(function (character) {
+        var pos = character.name.indexOf('(MAA)');
+        character.name = character.name.substr(0, pos - 1);
+    });
+    res.send(json);
 });
 
 module.exports = router;
