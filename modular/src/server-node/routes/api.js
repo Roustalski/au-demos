b4bd3e724313578e5ba88a632d28382d40e1df34
Russ Watson <roustalski@hotmail.com>
2016-01-18 18:22:35 -0600

    Node server mod to allow CORS for the karma test runner

--- modular/src/server-node/routes/api.js
+++ modular/src/server-node/routes/api.js
@@ -8,7 +8,12 @@ router.get('/maa', function (req, res, next) {
         var pos = character.name.indexOf('(MAA)');
         character.name = character.name.substr(0, pos - 1);
     });
-    res.send(json);
+    //Allow the test runner to access this API
+    res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Credentials', true);
+    // res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.json(json[0].data.results);
 });
 
 module.exports = router;
