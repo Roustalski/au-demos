be27d58bb947f1aa7a696b62f195b48d2053b8a5
Russ Watson <roustalski@hotmail.com>
2016-01-21 15:51:24 -0600

    Continued port of the angular modular project

--- modular/src/client/app/main.js
+++ modular/src/client/app/main.js
@@ -1,5 +1,7 @@
 import {UnhandledExceptionProvider} from 'app-exception-provider';
 import {UnhandledExceptionHandler} from 'app-exception-handler';
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
 import 'toastr';
 import 'bootstrap';
 
@@ -18,9 +20,35 @@ export function configure(aurelia) {
         aurelia.container.get(UnhandledExceptionProvider);
         aurelia.container.get(UnhandledExceptionHandler);
 
+        aurelia.container.registerSingleton(HttpClient, HttpClient);
+        configureHttpClient(aurelia.container.get(HttpClient));
+
         aurelia.setRoot('app/layout/shell');
     });
 
     toastr.options.timeOut = 4000;
     toastr.options.positionClass = 'toast-bottom-right';
 };
+
+function configureHttpClient(httpClient) {
+    httpClient.configure(config => {
+        config
+            .withBaseUrl('http://localhost:8080/')
+            .withDefaults({
+                headers: {
+                    'Accept': 'application/json',
+                    'X-Requested-With': 'Fetch'
+                }
+            })
+            .withInterceptor({
+                request(request) {
+                    console.log(`Requesting ${request.method} ${request.url}`);
+                    return request; // you can return a modified Request, or you can short-circuit the request by returning a Response
+                },
+                response(response) {
+                    console.log(`Received ${response.status} ${response.url}`);
+                    return response; // you can return a modified Response
+                }
+            });
+    });
+}
