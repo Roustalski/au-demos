e05e3689ff2d897313b875deb8a933882e6ec888
Russ Watson <roustalski@hotmail.com>
2016-01-22 16:37:22 -0600

    Added in the aurelia-velocity configuration

--- modular/src/client/config.js
+++ modular/src/client/config.js
@@ -33,6 +33,7 @@ System.config({
     "aurelia-templating-binding": "npm:aurelia-templating-binding@1.0.0-beta.1.0.2",
     "aurelia-templating-resources": "npm:aurelia-templating-resources@1.0.0-beta.1.0.4",
     "aurelia-templating-router": "npm:aurelia-templating-router@1.0.0-beta.1.0.5",
+    "aurelia/animator-velocity": "github:aurelia/animator-velocity@0.3.0",
     "babel": "npm:babel-core@5.8.34",
     "babel-runtime": "npm:babel-runtime@5.8.34",
     "bootstrap": "github:twbs/bootstrap@3.3.6",
@@ -47,6 +48,47 @@ System.config({
     "github:CodeSeven/toastr@2.1.2": {
       "css": "github:systemjs/plugin-css@0.1.20",
       "jquery": "github:components/jquery@2.2.0"
+    },
+    "github:aurelia/animator-velocity@0.3.0": {
+      "aurelia-metadata": "github:aurelia/metadata@0.10.1",
+      "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.0.2",
+      "aurelia-templating": "github:aurelia/templating@0.17.5",
+      "jsol": "github:daepark/JSOL@master",
+      "velocity": "npm:velocity-animate@1.2.3"
+    },
+    "github:aurelia/binding@0.11.4": {
+      "aurelia-metadata": "github:aurelia/metadata@0.10.1",
+      "aurelia-pal": "github:aurelia/pal@0.3.0",
+      "aurelia-task-queue": "github:aurelia/task-queue@0.9.0",
+      "core-js": "npm:core-js@1.2.6"
+    },
+    "github:aurelia/dependency-injection@0.12.1": {
+      "aurelia-logging": "github:aurelia/logging@0.9.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.10.1",
+      "aurelia-pal": "github:aurelia/pal@0.3.0",
+      "core-js": "npm:core-js@1.2.6"
+    },
+    "github:aurelia/loader@0.11.0": {
+      "aurelia-metadata": "github:aurelia/metadata@0.10.1",
+      "aurelia-path": "github:aurelia/path@0.11.0"
+    },
+    "github:aurelia/metadata@0.10.1": {
+      "aurelia-pal": "github:aurelia/pal@0.3.0",
+      "core-js": "npm:core-js@1.2.6"
+    },
+    "github:aurelia/task-queue@0.9.0": {
+      "aurelia-pal": "github:aurelia/pal@0.3.0"
+    },
+    "github:aurelia/templating@0.17.5": {
+      "aurelia-binding": "github:aurelia/binding@0.11.4",
+      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.12.1",
+      "aurelia-loader": "github:aurelia/loader@0.11.0",
+      "aurelia-logging": "github:aurelia/logging@0.9.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.10.1",
+      "aurelia-pal": "github:aurelia/pal@0.3.0",
+      "aurelia-path": "github:aurelia/path@0.11.0",
+      "aurelia-task-queue": "github:aurelia/task-queue@0.9.0",
+      "core-js": "npm:core-js@1.2.6"
     },
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
@@ -221,6 +263,10 @@ System.config({
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:velocity-animate@1.2.3": {
+      "jquery": "npm:jquery@2.2.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
